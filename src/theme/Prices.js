753aa315f74bb75dceb60969c42d3bd2836029ea
mahaupt <marcel.haupt@gepardec.com>
2021-08-20 22:36:31 +0200

    Fix prices flex

--- src/theme/Prices.js
+++ src/theme/Prices.js
@@ -29,65 +29,59 @@ class Prices extends Component {
   render() { 
     return (
       <div className="container mt-5">
-        <div class="flex-row justify-content-center">
+        <div class="row justify-content-center">
 
-          <div class="col-12 mb-5 card-prices">
-            <div class="card bg-light shadow-sm">
-              <div class="card-body">
-                <h3 className="text-center">Light</h3>
-                <h4 className="text-center fw-light text-muted">kostenlos</h4>
-                <table>
-                  {this.tableRow("Tische", true, "5")}
-                  {this.tableRow("Digitale Speisekarte", true)}
-                  {this.tableRow("Abrufe pro Tag", true, "30k")}
-                  {this.tableRow("Digitales Bestellen", false)}
+          <div class="col-md-auto mb-5 card-prices card bg-light">
+            <div class="card-body">
+              <h3 className="text-center">Light</h3>
+              <h4 className="text-center fw-light text-muted">kostenlos</h4>
+              <table>
+                {this.tableRow("Tische", true, "5")}
+                {this.tableRow("Digitale Speisekarte", true)}
+                {this.tableRow("Abrufe pro Tag", true, "30k")}
+                {this.tableRow("Digitales Bestellen", false)}
 
-                  {this.tableRow("Direktes Bezahlen", false)}
-                  {this.tableRow("Personal Dashboard", false)}
-                  {this.tableRow("Automatische Rechnungserstellung", false)}
-                  {this.tableRow("Automatische Abrechnung", false)}
-                </table>
-              </div>
+                {this.tableRow("Direktes Bezahlen", false)}
+                {this.tableRow("Personal Dashboard", false)}
+                {this.tableRow("Automatische Rechnungserstellung", false)}
+                {this.tableRow("Automatische Abrechnung", false)}
+              </table>
             </div>
           </div>
 
-          <div class="col-12 mb-5 card-prices">
-            <div class="card bg-light shadow-sm">
-              <div class="card-body">
-                <h3 className="text-center">Standard</h3>
-                <h4 className="text-center">109€ <span className="small text-muted fw-light">/ Monat</span></h4>
-                <table>
-                  {this.tableRow("Tische", true, "50")}
-                  {this.tableRow("Digitale Speisekarte", true)}
-                  {this.tableRow("Abrufe pro Tag", true, "∞")}
-                  {this.tableRow("Digitales Bestellen", true)}
+          <div class="col-md-auto mb-5 card-prices card bg-light">
+            <div class="card-body">
+              <h3 className="text-center">Standard</h3>
+              <h4 className="text-center">109€ <span className="small text-muted fw-light">/ Monat</span></h4>
+              <table>
+                {this.tableRow("Tische", true, "50")}
+                {this.tableRow("Digitale Speisekarte", true)}
+                {this.tableRow("Abrufe pro Tag", true, "∞")}
+                {this.tableRow("Digitales Bestellen", true)}
 
-                  {this.tableRow("Direktes Bezahlen", false)}
-                  {this.tableRow("Personal Dashboard", true)}
-                  {this.tableRow("Automatische Rechnungserstellung", false)}
-                  {this.tableRow("Automatische Abrechnung", false)}
-                </table>
-              </div>
+                {this.tableRow("Direktes Bezahlen", false)}
+                {this.tableRow("Personal Dashboard", true)}
+                {this.tableRow("Automatische Rechnungserstellung", false)}
+                {this.tableRow("Automatische Abrechnung", false)}
+              </table>
             </div>
           </div>
 
-          <div class="col-12 mb-5 card-prices">
-            <div class="card bg-light shadow-sm">
-              <div class="card-body">
-                <h3 className="text-center">Premium</h3>
-                <h4 className="text-center">229€ <span className="small text-muted fw-light">/ Monat</span></h4>
-                <table>
-                  {this.tableRow("Tische", true, "200")}
-                  {this.tableRow("Digitale Speisekarte", true)}
-                  {this.tableRow("Abrufe pro Tag", true, "∞")}
-                  {this.tableRow("Digitales Bestellen", true)}
+          <div class="col-md-auto mb-5 card-prices card bg-light">
+            <div class="card-body">
+              <h3 className="text-center">Premium</h3>
+              <h4 className="text-center">229€ <span className="small text-muted fw-light">/ Monat</span></h4>
+              <table>
+                {this.tableRow("Tische", true, "200")}
+                {this.tableRow("Digitale Speisekarte", true)}
+                {this.tableRow("Abrufe pro Tag", true, "∞")}
+                {this.tableRow("Digitales Bestellen", true)}
 
-                  {this.tableRow("Direktes Bezahlen", true)}
-                  {this.tableRow("Personal Dashboard", true)}
-                  {this.tableRow("Automatische Rechnungserstellung", true)}
-                  {this.tableRow("Automatische Abrechnung", true)}
-                </table>
-              </div>
+                {this.tableRow("Direktes Bezahlen", true)}
+                {this.tableRow("Personal Dashboard", true)}
+                {this.tableRow("Automatische Rechnungserstellung", true)}
+                {this.tableRow("Automatische Abrechnung", true)}
+              </table>
             </div>
           </div>
 
