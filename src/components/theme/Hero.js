73cc36663ad3715687caf8b696cada07a1b3cb6d
mahaupt <subcortexx@gmail.com>
2022-04-16 18:30:34 +0200

    Add demo link to hero

--- src/components/theme/Hero.js
+++ src/components/theme/Hero.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useMatomo } from '@datapunt/matomo-tracker-react';
 import { HashLink as Link } from 'react-router-hash-link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import './Hero.scss';
 import mobile from '../../assets/mobile-4.png';
 
 export default function Hero(props) {
   const { trackEvent } = useMatomo();
+  const testLink = "https://app.bestello.at/?t=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiZ3Vlc3QiLCJ1c2VyX2lkIjoiIiwicmVzdGF1cmFudF9pZCI6IjYxOTY5NjFiMmE1ZTUwMjc0NmRjOTY1MyIsInRhYmxlX2lkIjoiMDAwMDAwIiwiZXhwaXJlc19hdCI6Ijk5OTktMDEtMDFUMTI6MDA6MDAuMDBaIn0.qdoaYY9cJPiiMNwh7qwRfzmfb9_HnnllRGn0Uq4bv_c0OnD4BzyaRePtsAxCQKhYGDNPY1r7gUFeCFd4ovNhPw";
 
   return ( 
     <div className="hero mb-70">
@@ -22,6 +25,9 @@ export default function Hero(props) {
             </p>
             <p>
               <Link to="/signup" onClick={() => trackEvent({ category: 'Landing', action: 'click', name: 'cta-hero' })} className="btn btn-light">Jetzt kostenlos testen!</Link>
+            </p>
+            <p>
+              <a href={testLink} onClick={() => trackEvent({ category: 'Landing', action: 'click', name: 'demo-hero' })} className="btn btn-warning" target="_blank" rel="noreferrer">Zur Demokarte <FontAwesomeIcon icon={faArrowRight}/></a>
             </p>
           </div>
           <div className="col-12 col-md-6 background-dots">
