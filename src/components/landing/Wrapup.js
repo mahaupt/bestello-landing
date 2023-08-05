4b08ef7962edefecfee15c81eee336e4cd2744a7
mahaupt <subcortexx@gmail.com>
2023-08-05 10:17:13 +0200

    Update node version

--- src/components/landing/Wrapup.js
+++ src/components/landing/Wrapup.js
@@ -6,7 +6,7 @@ export default function Wrapup(props) {
   const { trackEvent } = useMatomo();
 
   return (
-    <div className="text-center bg-light mt-5 mb-5 pt-5 pb-5">
+    <div className="text-center mt-5 mb-5 pt-5 pb-5">
       <h2 className="mb-3 fw-light">Die digitale Speisekarte der Zukunft</h2>
       <p><Link to="/signup" onClick={() => trackEvent({ category: 'Landing', action: 'click', name: 'cta-wrapup' })} className="btn btn btn-dark">Jetzt kostenlos anmelden!</Link></p>
     </div>
